da3243017a4a4e7fbd4d66755644113f31ad5edb
Jonatan Machado <jonatanmachado35@gmail.com>
2019-07-04 19:47:25 -0300

    estilizado todo o sidebar e o player

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import GlobalStyle from './styles/global';
+
+import Sidebar from './Components/Sidebar/index';
+import Player from './Components/Player/index';
+import { Wrapper, Container } from './styles/components';
+
+const App = () => (
+  <Wrapper>
+    <GlobalStyle />
+    <Container>
+      <Sidebar />
+    </Container>
+    <Player />
+  </Wrapper>
+);
 
 export default App;
